98f5765ecafecf5f5a1d0d899c5b68aa6cc21ddc
Nikhil Shisode <niksde7@gmail.com>
2024-01-28 22:48:02 +0530

    04-jpa-repository

---
+++ 04-rest-crud-apis/04-02-rest-crud-employee-jpa-repository/src/main/java/com/niksde/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,50 @@
+package com.niksde.springboot.cruddemo.service;
+
+import com.niksde.springboot.cruddemo.dao.EmployeeRepository;
+import com.niksde.springboot.cruddemo.entity.Employee;
+import com.niksde.springboot.cruddemo.util.EmployeeNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository theEmployeeRepository) {
+        employeeRepository = theEmployeeRepository;
+    }
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        Optional<Employee> result = employeeRepository.findById(theId);
+        Employee theEmployee = null;
+        if(result.isPresent()) {
+            theEmployee = result.get();
+        } else {
+            throw new EmployeeNotFoundException("Did not find employee id - "+ theId);
+        }
+        return theEmployee;
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeRepository.save(theEmployee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeRepository.deleteById(theId);
+    }
+}
