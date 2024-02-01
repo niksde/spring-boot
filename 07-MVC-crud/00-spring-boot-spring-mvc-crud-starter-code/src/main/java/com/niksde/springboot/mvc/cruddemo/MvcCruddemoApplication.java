d060ed644cc672cc519d60a8ef05124c7ce3d4ad
Nikhil Shisode <niksde7@gmail.com>
2024-02-02 01:24:24 +0530

    mvc-crud

---
+++ 07-MVC-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/niksde/springboot/mvc/cruddemo/MvcCruddemoApplication.java
@@ -0,0 +1,13 @@
+package com.niksde.springboot.mvc.cruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcCruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcCruddemoApplication.class, args);
+	}
+
+}
