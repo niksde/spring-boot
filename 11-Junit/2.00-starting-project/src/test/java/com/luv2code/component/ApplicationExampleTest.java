5875bde649ee71d9046682589c5bb2e296582957
Nikhil Shisode <niksde7@gmail.com>
2024-02-05 19:55:17 +0530

    Spring boot unit testing

---
+++ 11-Junit/2.00-starting-project/src/test/java/com/luv2code/component/ApplicationExampleTest.java
@@ -0,0 +1,12 @@
+package com.luv2code.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationExampleTest {
+    @Test
+    void basicTest() {
+
+    }
+}
